b8f5a5d31cff3f222fc65f18e4195892d52dd35b
Bfunk54 <ben_fein@icloud.com>
2022-11-23 14:22:11 -0500

    added more to the employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,15 +1,33 @@
 // Require the employee class
 const Employee = require("./Employee");
 
-describe('Employee class', () => {
-  it('Creates an Employee object with all constructors', () => {
-    let name = 'Darth Vader';
+describe("Employee class", () => {
+  it("Creates an Employee object with all constructors", () => {
+    let name = "Darth Vader";
     let id = 1;
-    let email = 'darthvader@sithlord.com';
+    let email = "darthvader@sithlord.com";
     const employee = new Employee(name, id, email);
 
-    expect(employee.name).toEqual(name)
-    expect(employee.id).toEqual(id)
-    expect(employee.email).toEqual(email)
-    });
+    expect(employee.name).toEqual(name);
+    expect(employee.id).toEqual(id);
+    expect(employee.email).toEqual(email);
   });
+});
+
+describe("Employee class getName method", () => {
+  it("Grabs and returns the employee name", () => {
+    let name = "Darth Vader";
+    const employee = new Employee(name);
+
+    expect(employee.name).toEqual(name);
+  });
+});
+
+describe("Employee class getId method", () => {
+  it("Grabs and returns the employee id", () => {
+    let id = 1;
+    const employee = new Employee(id);
+
+    expect(employee.id).toEqual(id);
+  });
+});
